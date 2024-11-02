e8eb5d5d2f8b2de83f958c62554b20fef4611672
therealmarvin <nakedtoothbrush@hotmail.com>
2024-11-02 19:11:38 -0400

    multigoal now working

--- main_multi_goal.py
+++ main_multi_goal.py
@@ -14,7 +14,7 @@ from action import Action
 from agent import Agent
 from event_manager import EventManager
 from goal import Goal
-from goap_planner import GOAPPlanner
+from goap_planner import GOAPPlanner, PlanningMode
 
 actions = [
     Action(name="ActionA", preconditions={}, effects={"condition_x": 1}, cost=100, duration=10),
@@ -23,12 +23,9 @@ actions = [
 ]
 
 
-def main(mode):
+def main():
     """
     Main function to handle the multi goals experiments based on the selected mode.
-
-    Args:
-        mode (str): The mode to run ('plan' to generate and display the plan, 'execute' to run the plan with dynamic events).
     """
     initial_state = {}
 
@@ -39,35 +36,18 @@ def main(mode):
     ]
 
     planner = GOAPPlanner(actions)
-    plan, total_cost = planner.plan(initial_state, goals, {})
 
-    if mode == "plan":
-        print(f"Generated Plan: {plan} with total cost: {total_cost}")
-        return
+    plan, total_cost = planner.plan(initial_state, goals, {}, mode=PlanningMode.GLOBAL)
+    print(f"Generated Plan (Global): {plan} with total cost: {total_cost}")
+    planner.display_usage_stats()
 
-    event_manager = EventManager()
-
-    def event_thread():
-        """
-        Simulates dynamic events by periodically publishing events through the event manager.
-        """
-        while True:
-            time.sleep(5)
-            event_manager.publish_event()
-
-    agent = Agent(actions, planner, event_manager, verbose=True)
-
-    event_thread_obj = threading.Thread(target=event_thread, daemon=True)
-    event_thread_obj.start()
-
-    agent.execute_plan(initial_state, plan, {"goals": goal})
+    plan, total_cost = planner.plan(initial_state, goals, {}, mode=PlanningMode.SEQUENTIAL)
+    print(f"Generated Plan (Sequential): {plan} with total cost: {total_cost}")
     planner.display_usage_stats()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Multi Goals Experiment")
-    parser.add_argument("--mode", choices=["plan", "execute"], default="plan",
-                        help="Choose whether to plan or execute the experiment")
     args = parser.parse_args()
 
-    main(args.mode)
+    main()
